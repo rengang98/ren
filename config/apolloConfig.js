2a8062796dfac3f826dd43fe6866e91dbbcca53e
rengang <rengang@wecash.net>
2019-03-08 11:13:24 +0800

    ren's practice 07/03/2019 接入阿波罗

---
+++ config/apolloConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    test:{
+        configServerUrl:'http://106.12.25.204:8070',
+        appId:'ren',
+        clusterName:"renCluster",
+        namespaceName:"renPractice",
+        token:'003bed22a80764366c4ef4439e2c0f016bc8dfd6',
+        apolloEnv:'DEV'
+    },
+}
